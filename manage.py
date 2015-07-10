336ad70179344a74f553b816cd531dc4364b776f
Sergey Tikhonov <zimbler@gmail.com>
2015-07-10 14:15:48 +0300

    fix pythonpath for console tests

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "testproject"))
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
 

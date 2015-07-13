54ca25e5fe39374d3db08b4387854eacf95fc0f6
Sergey Tikhonov <zimbler@gmail.com>
2015-07-13 10:33:15 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django_sphinxsearch',
+    version='0.0.1',
+    packages=['sphinxsearch'],
+    url='http://github.com/tumb1er/django_sphinxsearch',
+    license='Beer License',
+    author='tumbler',
+    author_email='zimbler@gmail.com',
+    description='Sphinxsearch database backend for django'
+)

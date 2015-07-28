3443d446abaf4e9eb4ff4de896c72514f5038741
Sergey Tikhonov <zimbler@gmail.com>
2015-07-28 13:44:59 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django_sphinxsearch',
-    version='0.0.1',
+    version='0.0.2',
     packages=[
         'sphinxsearch',
         'sphinxsearch.backend',

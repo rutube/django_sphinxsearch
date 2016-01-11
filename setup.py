f29f23414a5b64266e2c2b2c19e7ca945cc0de8a
tumb1er <zimbler@gmail.com>
2016-01-11 10:41:52 +0300

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except ImportError:
 
 setup(
     name='django_sphinxsearch',
-    version='0.1.0',
+    version='0.2.0',
     long_description=read_md('README.md'),
     packages=[
         'sphinxsearch',
